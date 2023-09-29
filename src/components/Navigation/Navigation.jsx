d8048d3f6fc8aa47ca08bdbc394a86a160757256
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-29 12:57:31 +0200

    used Logo Menu components, and added few class css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,16 @@
+import Logo from "../Logo/Logo";
+import Menu from "../Menu/Menu";
 import "./navigation.scss";
 
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <header className="page-header">
+      <div className="container">
+        <nav className="nav">
+          <Logo />
+          <Menu />
+        </nav>
+      </div>
+    </header>
+  );
 }
