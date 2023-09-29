edca9342b03f1c1b204884a7454bf924b8c5c6c2
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-29 12:14:16 +0200

    used Navigation in App and created Logo component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "./styles/global.scss";
 import { Suspense, lazy } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 
+// pages
 const Home = lazy(() => import("./pages/Home/Home"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound/PageNotFound"));
+
+// components
+const Navigation = lazy(() => import("./components/Navigation/Navigation"));
 
 export default function App() {
   return (
     <div id="page" className="page">
+      <Navigation />
       <Suspense>
         <Routes>
           <Route path="/" exact element={<Home />} />
