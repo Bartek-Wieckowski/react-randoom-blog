0907f267feb577f95f96d4b8555013f21a15d663
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-04 11:12:16 +0200

    used props,func and event click, completed styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,13 +2,20 @@ import Logo from "../Logo/Logo";
 import Menu from "../Menu/Menu";
 import "./navigation.scss";
 
-export default function Navigation() {
+export default function Navigation({ onOpenMobileMenu }) {
+  const handleClickMenu = () => {
+    onOpenMobileMenu(true);
+  };
   return (
     <header className="page-header">
       <div className="container">
         <nav className="nav">
           <Logo />
           <Menu />
+          <button type="button" className="menu-trigger" onClick={handleClickMenu}>
+            <i className="ri-menu-3-line"></i>
+            <i className="ri-close-line"></i>
+          </button>
         </nav>
       </div>
     </header>
