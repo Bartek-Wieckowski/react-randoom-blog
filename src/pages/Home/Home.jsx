166e49c2e6e1904b62e5fa41b46f9632acb5193c
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-04 15:33:09 +0200

    used for test MainWrapper component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import MainWrapper from "../../components/MainWrapper/MainWrapper";
 import Slider from "../../components/Slider/Slider";
 import "./home.scss";
 
@@ -5,6 +6,9 @@ export default function Home() {
   return (
     <section className="home">
         <Slider />
+        <MainWrapper>
+          test
+        </MainWrapper>
     </section>
   );
 }
