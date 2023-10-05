2da8875e1b4a9fb1baae557dd5e860a0c580c324
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-05 16:09:22 +0200

    added fixed navigation when scroll page is more than 600

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from "react";
 import Logo from "../Logo/Logo";
 import Menu from "../Menu/Menu";
 import Search from "../Search/Search";
 import "./navigation.scss";
 
 export default function Navigation({ onOpenMobileMenu }) {
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 600) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   const handleClickMenu = () => {
     onOpenMobileMenu(true);
     document.body.classList.toggle('overflowme');
   };
 
+  const navFixedClass = isFixed ? 'fixed' : '';
+
   return (
-    <header className="page-header">
+    <header className={`page-header ${navFixedClass}`}>
       <div className="container">
         <nav className="nav">
           <Logo />
