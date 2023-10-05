0fdde84c912640ca7e9a3f8bb22f2c969c9a6cea
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-05 16:00:34 +0200

    used PostList and PostPreview component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
 import MainWrapper from "../../components/MainWrapper/MainWrapper";
+import PostPreview from "../../components/PostPreview/PostPreview";
+import PostsList from "../../components/PostsList/PostsList";
 import Slider from "../../components/Slider/Slider";
 import "./home.scss";
 
 export default function Home() {
   return (
     <section className="home">
-        <Slider />
-        <MainWrapper>
-          <h3 className="home__title">Najnowsze posty</h3>
-        </MainWrapper>
+      <Slider />
+      <MainWrapper>
+        <h3 className="home__title">Najnowsze posty</h3>
+        <PostsList>
+          <PostPreview />
+        </PostsList>
+      </MainWrapper>
     </section>
   );
 }
