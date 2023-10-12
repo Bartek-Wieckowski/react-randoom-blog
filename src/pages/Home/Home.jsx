93519fe579c31442f010f21db1a4e0710a40d389
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-12 13:35:31 +0200

    passed right data props to component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,19 @@ import MainWrapper from "../../components/MainWrapper/MainWrapper";
 import PostPreview from "../../components/PostPreview/PostPreview";
 import PostsList from "../../components/PostsList/PostsList";
 import Slider from "../../components/Slider/Slider";
+import { usePosts } from "../../contexts/PostsContext";
 import "./home.scss";
 
 export default function Home() {
+  const { postsPreview, isLoading } = usePosts();
+
   return (
     <section className="home">
       <Slider />
       <MainWrapper>
         <h3 className="home__title">Najnowsze posty</h3>
         <PostsList>
-          <PostPreview />
+          <PostPreview postsData={postsPreview} isLoading={isLoading} />
         </PostsList>
       </MainWrapper>
     </section>
