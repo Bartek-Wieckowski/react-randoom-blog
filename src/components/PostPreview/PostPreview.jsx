8b2d9aee0fa6fde25f92033db71fe22d30b72ca6
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-12 12:02:00 +0200

    update contentful fields and again test to fetch this data into post preview

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./post-preview.scss";
 import clientContentful from "../../helpers/contentfulConfig";
 import { useEffect, useState } from "react";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default function PostPreview() {
   const [loading, setLoading] = useState(true);
@@ -12,12 +13,12 @@ export default function PostPreview() {
       const response = await clientContentful.getEntries({
         content_type: "randoomBlogPosts",
       });
-      const posts = response.items.map((item) => {
-        const { title, author, category } = item.fields;
-        return { title, author, category };
+      const postsData = response.items.map((item) => {
+        const { title, author, category, contentPreview, readTime } = item.fields;
+        return { title, author, category, contentPreview, readTime };
       });
       setLoading(false);
-      setPosts(posts);
+      setPosts(postsData);
     } catch (error) {
       console.log(error);
       setLoading(false);
@@ -43,32 +44,19 @@ export default function PostPreview() {
                 <Link to="/">{post.category}</Link>
               </span>
               <span className="read-time">
-                <Link to="/">1 min</Link>
+                <Link to="/">{post.readTime} min</Link>
               </span>
               <span className="comment">
-                <Link to="/">3 Comments</Link>
+                <Link to="/">3 komentarze</Link>
               </span>
             </div>
           </header>
           <section className="post-preview__content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dignissimos laborum vitae ea
-              architecto iure quis numquam impedit. Iusto, laboriosam? Earum eos expedita provident, ea
-              commodi aperiam consequatur esse, corrupti assumenda aliquid inventore consequuntur accusantium,
-              architecto error vitae repellendus culpa quam saepe cum ullam autem dolorem nam! Ullam ea
-              similique, porro laborum eligendi architecto vel quaerat error ex qui sint quis eaque
-              perferendis ab ipsam, voluptatum esse ut officia debitis repellendus repudiandae enim
-              consectetur incidunt! Soluta beatae est, reiciendis quaerat expedita dignissimos incidunt, nihil
-              voluptate ab nesciunt maxime quos. Odit obcaecati nam totam, minus possimus nisi. Modi, cumque
-              quibusdam! Quibusdam quas placeat libero nemo, molestias dolor iste sint, voluptatibus amet quis
-              ducimus harum unde voluptates aperiam porro? Iure ea nam veniam impedit repellat, voluptas sit
-              omnis quisquam soluta accusantium expedita quos rerum similique dolore quas perspiciatis
-              repellendus sequi, corporis animi.
-            </p>
+            {documentToReactComponents(post.contentPreview)}
           </section>
           <footer>
             <Link to="/" className="post-preview__btn read-more">
-              Read more
+              Czytaj więcej
             </Link>
           </footer>
         </article>
