a46efab9204a4f248ab3df12e79f400854584508
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-12 15:36:53 +0200

    completed jsx,fetch and css popular component

--- src/pages/Popular/Popular.jsx
+++ src/pages/Popular/Popular.jsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import PostPreview from "../../components/PostPreview/PostPreview";
+import PostsList from "../../components/PostsList/PostsList";
+import { usePosts } from "../../contexts/PostsContext";
 import "./popular.scss";
+import MainWrapper from "../../components/MainWrapper/MainWrapper";
 
 export default function Popular() {
-  return <div>Popular</div>;
+  const { postsPopular, isLoading, fetchPopularPosts } = usePosts();
+
+  useEffect(() => {
+    fetchPopularPosts();
+  }, [fetchPopularPosts]);
+
+  return (
+    <section className="popular">
+      <section className="hero">
+        <div className="hero__popular">
+          <h2>Poznaj najbardziej popularne posty</h2>
+          <h2>wg naszych czytelników</h2>
+        </div>
+      </section>
+      <MainWrapper>
+        <h3 className="popular__title">Popularne posty</h3>
+        <PostsList>
+          <PostPreview postsData={postsPopular} isLoading={isLoading} />
+        </PostsList>
+      </MainWrapper>
+    </section>
+  );
 }
