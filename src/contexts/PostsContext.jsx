f52916da4ef61893e290daaf9cb84b462f4fc964
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-12 15:35:49 +0200

    created for popular page: post array, reducer action anf fetch function

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { createContext, useCallback, useContext, useEffect, useReducer } from "react";
 import contentfulClient from "../helpers/contentfulConfig";
 
 const PostsContext = createContext();
@@ -6,6 +6,7 @@ const PostsContext = createContext();
 const initialState = {
   posts: [],
   postsPreview: [],
+  postsPopular: [],
   isLoading: false,
   error: "",
 };
@@ -18,13 +19,15 @@ function reducer(state, action) {
       return { ...state, isLoading: false, error: action.payload };
     case "postsPreview/loaded":
       return { ...state, isLoading: false, postsPreview: action.payload };
+    case "postsPopular/loaded":
+      return { ...state, isLoading: false, postsPopular: action.payload };
     default:
       throw new Error("Unknown action type");
   }
 }
 
 function PostsProvider({ children }) {
-  const [{ postsPreview, isLoading }, dispatch] = useReducer(reducer, initialState);
+  const [{ postsPreview, isLoading, postsPopular }, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     async function fetchPosts() {
@@ -45,7 +48,28 @@ function PostsProvider({ children }) {
     fetchPosts();
   }, []);
 
-  return <PostsContext.Provider value={{ postsPreview, isLoading }}>{children}</PostsContext.Provider>;
+  const fetchPopularPosts = useCallback(async () => {
+    dispatch({ type: "loading" });
+    try {
+      const response = await contentfulClient.getEntries({
+        content_type: "randoomBlogPosts",
+        "fields.popular": true,
+      });
+      const postsData = response.items.map((item) => {
+        const { title, author, category, contentPreview, readTime } = item.fields;
+        return { title, author, category, contentPreview, readTime };
+      });
+      dispatch({ type: "postsPopular/loaded", payload: postsData });
+    } catch (error) {
+      dispatch({ type: "rejected", payload: "There was an error loading popular posts" });
+    }
+  }, []);
+
+  return (
+    <PostsContext.Provider value={{ postsPreview, isLoading, fetchPopularPosts, postsPopular }}>
+      {children}
+    </PostsContext.Provider>
+  );
 }
 
 function usePosts() {
