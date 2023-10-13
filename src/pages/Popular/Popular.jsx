a27b0952f2559102bba7d5029baa56a47250faa5
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-13 16:19:06 +0200

    added function which scroll to section after click event

--- src/pages/Popular/Popular.jsx
+++ src/pages/Popular/Popular.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import PostPreview from "../../components/PostPreview/PostPreview";
 import PostsList from "../../components/PostsList/PostsList";
 import { usePosts } from "../../contexts/PostsContext";
@@ -8,21 +8,36 @@ import Hero from "../../components/Hero/Hero";
 
 export default function Popular() {
   const { postsPopular, isLoading, fetchPopularPosts } = usePosts();
+  const postsElement = useRef(null);
 
   useEffect(() => {
     fetchPopularPosts();
   }, [fetchPopularPosts]);
 
+  const scrollToTarget = () => {
+    if (postsElement.current) {
+      const targetElement = postsElement.current;
+      const targetPosition = targetElement.getBoundingClientRect().top;
+
+      window.scrollTo({
+        top: targetPosition - 125,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <section className="popular">
       <Hero type="hero__normal">
         <div className="hero__normal-titles">
-          <h2>Poznaj najbardziej popularne posty wg naszych czytelników</h2>
+          <h2 onClick={() => scrollToTarget()}>Poznaj najbardziej popularne posty wg naszych czytelników</h2>
         </div>
       </Hero>
 
       <MainWrapper>
-        <h3 className="popular__title">Popularne posty</h3>
+        <h3 className="popular__title" ref={postsElement}>
+          Popularne posty
+        </h3>
         <PostsList>
           <PostPreview postsData={postsPopular} isLoading={isLoading} />
         </PostsList>
