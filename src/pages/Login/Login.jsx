30d79a8831ba824e869c115a22f816cc1719e7dd
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-20 15:52:02 +0200

    added css style in wrapper and used Form,Register component inside page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,12 @@
-import "./login.scss"
+import "./login.scss";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 export default function Login() {
   return (
-    <section className="login">Login</section>
-  )
+    <section className="login">
+      <div className="login__form-wrapper">
+        <LoginForm />
+      </div>
+    </section>
+  );
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,12 @@
 import "./register.scss";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
 export default function Register() {
   return (
     <section className="register">
-      Register
+      <div className="register__form-wrapper">
+        <RegisterForm />
+      </div>
     </section>
   );
 }
