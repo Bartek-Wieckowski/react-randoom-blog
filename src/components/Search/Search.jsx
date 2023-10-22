b2a6ec5a2b54ae0b2d0e3aa8fa20edc9ae1e7701
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-22 13:50:58 +0200

    added  query in url, maked input search controlled element

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,63 @@
-import { useRef } from "react";
 import "./search.scss";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// TODO: do zrobienia autofocus w input search
 
 export default function Search() {
-  const elementSearch = useRef(null);
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const elementSearchRef = useRef(null);
+
+  const handleSearch = () => {
+    if (query.trim() === "") {
+      return; // TODO: dodac alert o pustym inpucie
+    }
+
+    navigate(`/wyniki-wyszukiwania?q=${query}`);
+    setQuery("");
+    toggleSearchClass();
+  };
+
+  const onEnterKeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (query.trim() !== "") {
+        handleSearch();
+      } else {
+        e.preventDefault();
+      }
+    }
+  };
+
   const toggleSearchClass = () => {
-    if (elementSearch.current) {
-      elementSearch.current.classList.toggle("showsearch");
-      document.body.classList.toggle('overflowme');
+    if (elementSearchRef.current) {
+      elementSearchRef.current.classList.toggle("showsearch");
+      document.body.classList.toggle("overflowme");
     }
   };
 
   return (
     <>
-      <button type="button" className="search-trigger" onClick={() => toggleSearchClass()}>
+      <button type="button" className="search-trigger" onClick={toggleSearchClass}>
         <i className="ri-search-line"></i>
       </button>
-      <div className="search" ref={elementSearch}>
-        <button type="button" className="search-close" onClick={() => toggleSearchClass()}>
+      <div className="search" ref={elementSearchRef}>
+        <button type="button" className="search-close" onClick={toggleSearchClass}>
           <i className="ri-close-line"></i>
         </button>
-        <form>
-          <input type="search" placeholder="Zacznij pisać i wciśnij enter :)" />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSearch();
+          }}
+        >
+          <input
+            type="search"
+            placeholder="Zacznij pisać i wciśnij enter :)"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={onEnterKeyPress}
+          />
         </form>
       </div>
     </>
