1b7268a61556468adb49276d2646dfe6cb93217d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-11 13:24:30 +0100

    created function which get data ang increase column form supabase

--- src/services/apiPosts.js
+++ src/services/apiPosts.js
@@ -16,3 +16,31 @@ export async function addPostIDsToSupabase(contentfulPostIDs) {
     console.error("Błąd podczas dodawania identyfikatorów postów do Supabase:", error.message);
   }
 }
+
+export async function selectCurrentPostFromSupabase(id) {
+  const { data, error } = await supabase
+    .from("randoomBlogPosts")
+    .select("viewCount")
+    .eq("postIDContentful", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bład podczas odczytu danych");
+  }
+
+  return data;
+}
+
+export async function updateCountViewInSupabase(id, viewCount) {
+  const { data, error } = await supabase
+    .from("randoomBlogPosts")
+    .update({ viewCount: viewCount })
+    .eq("postIDContentful", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bład podczas odczytu danych");
+  }
+
+  return data;
+}
