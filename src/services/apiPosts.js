0d3d785eac2e780b691eca54c5eec659db93bbf7
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-12 10:54:04 +0100

    fix: function which added id contenful post to supabase

--- src/services/apiPosts.js
+++ src/services/apiPosts.js
@@ -2,15 +2,22 @@ import supabase from "../utils/supabaseConfig";
 
 export async function addPostIDsToSupabase(contentfulPostIDs) {
   try {
-    for (const contentfulPostID of contentfulPostIDs) {
-      const existingRecord = await supabase
-        .from("randoomBlogPosts")
-        .select("postIDContentful")
-        .eq("postIDContentful", contentfulPostID);
+    const existingRecords = await supabase
+      .from("randoomBlogPosts")
+      .select("postIDContentful")
+      .in("postIDContentful", contentfulPostIDs);
 
-      if (existingRecord.data.length === 0) {
-        await supabase.from("randoomBlogPosts").upsert([{ postIDContentful: contentfulPostID }]);
-      }
+    const newPostIDs = contentfulPostIDs.filter(
+      (contentfulPostID) =>
+        !existingRecords.data.some((record) => record.postIDContentful === contentfulPostID)
+    );
+
+    if (newPostIDs.length > 0) {
+      const newRecords = newPostIDs.map((contentfulPostID) => ({
+        postIDContentful: contentfulPostID,
+      }));
+
+      await supabase.from("randoomBlogPosts").upsert(newRecords);
     }
   } catch (error) {
     console.error("Błąd podczas dodawania identyfikatorów postów do Supabase:", error.message);
