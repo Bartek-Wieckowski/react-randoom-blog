605a65757b36f6231b39e3a1a9521e9654abc386
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-14 13:08:06 +0100

    add first css and jsx to register/login form component

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,22 @@
 import "./login-form.scss";
 
 export default function LoginForm() {
-  return <div>LoginForm</div>;
+  return (
+    <div className="login-form">
+      <h3>Zaloguj się</h3>
+      <form action="">
+        <div className="wrapper">
+          <p>
+            <input type="email" placeholder="Email" />
+          </p>
+          <p>
+            <input type="password" placeholder="Hasło" required />
+          </p>
+        </div>
+        <p className="co-submit">
+          <input type="submit" value="Zaloguj" className="btn submit read-more" />
+        </p>
+      </form>
+    </div>
+  );
 }
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,7 +1,34 @@
-import "./register-form.scss"
+import "./register-form.scss";
 
 export default function RegisterForm() {
   return (
-    <div>RegisterForm</div>
-  )
+    <div className="register-form">
+      <h3>Zarejestruj się</h3>
+      <form action="">
+        <div className="wrapper">
+          <p>
+            <input type="text" placeholder="Imię" required />
+          </p>
+          <p>
+            <input type="text" placeholder="Nazwisko" required />
+          </p>
+          <p>
+            <input type="text" placeholder="Nickname" required />
+          </p>
+          <p>
+            <input type="email" placeholder="Email" />
+          </p>
+          <p>
+            <input type="password" placeholder="Hasło" required />
+          </p>
+          <p>
+            <input type="password" placeholder="Powtórz hasło" required />
+          </p>
+        </div>
+        <p className="co-submit">
+          <input type="submit" value="Wyślij" className="btn submit read-more" />
+        </p>
+      </form>
+    </div>
+  );
 }
