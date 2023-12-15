f1fbb1c004f7be67ee596a51aa63636ddde826d8
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-15 11:08:31 +0100

    add query client provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Toaster position="top-right" reverseOrder={false} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
