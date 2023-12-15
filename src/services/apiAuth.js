b9df59a5e9d7750c36929c02e44cfdd4ffb94eeb
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-15 12:23:05 +0100

    add login function

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -16,3 +16,14 @@ export async function signupApi({ fullName, email, password }) {
   }
   return data;
 }
+
+export async function loginApi({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data;
+}
