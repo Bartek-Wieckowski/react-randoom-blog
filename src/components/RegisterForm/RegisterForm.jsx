1ee67678e00dd5605531c8b1bde2a2f9b8d401be
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-15 11:10:23 +0100

    complete registser form with simple errors display and validate

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,26 +1,78 @@
 import './register-form.scss';
+import { useForm } from 'react-hook-form';
+import { useRegister } from './useRegisterHook';
 
 export default function RegisterForm() {
+  const { register: signup, isLoading } = useRegister();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
+  const { errors } = formState;
+
+  const handleSignUpForm = ({ fullName, email, password }) => {
+    signup({ fullName, email, password }, { onSettled: () => reset() });
+  };
+
   return (
     <div className="register-form">
       <h3>Zarejestruj się</h3>
-      <form action="">
+      <form onSubmit={handleSubmit(handleSignUpForm)}>
         <div className="wrapper">
-          <p>
-            <input type="text" placeholder="Imię i nazwisko" required />
-          </p>
-          <p>
-            <input type="text" placeholder="Nickname" required />
-          </p>
-          <p>
-            <input type="email" placeholder="Email" />
-          </p>
-          <p>
-            <input type="password" placeholder="Hasło" required />
-          </p>
-          <p>
-            <input type="password" placeholder="Powtórz hasło" required />
-          </p>
+          <div>
+            <input
+              type="text"
+              id="fullName"
+              placeholder="Imię i nazwisko"
+              {...register('fullName', {
+                required: 'To pole jest obowiązkowe',
+              })}
+            />
+            <p className="error-text">{errors?.fullName?.message}</p>
+          </div>
+          <div>
+            <input type="text" id="nickname" placeholder="Nickname" />
+          </div>
+          <div>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              {...register('email', {
+                required: 'To pole jest obowiązkowe',
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: 'Proszę podać prawidłowy e-mail',
+                },
+              })}
+            />
+            <p className="error-text">{errors?.email?.message}</p>
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              placeholder="Hasło"
+              {...register('password', {
+                required: 'To pole jest obowiązkowe',
+                minLength: {
+                  value: 8,
+                  message: 'Hasło musi mieć conajmniej 8 znaków',
+                },
+              })}
+            />
+            <p className="error-text">{errors?.password?.message}</p>
+          </div>
+          <div>
+            <input
+              type="password"
+              id="passwordConfirm"
+              placeholder="Powtórz hasło"
+              {...register('passwordConfirm', {
+                required: 'To pole jest obowiązkowe',
+                validate: (value) =>
+                  value === getValues().password || 'Hasła nie są takie same',
+              })}
+            />
+            <p className="error-text">{errors?.passwordConfirm?.message}</p>
+          </div>
         </div>
         <p className="co-submit">
           <input
