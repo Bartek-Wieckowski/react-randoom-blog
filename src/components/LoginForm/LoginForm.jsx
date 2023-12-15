f4fd757b691e6c2aeb0cbc3d3b6aa3b312dc8009
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-15 12:24:10 +0100

    create login with validation input

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,21 +1,91 @@
-import "./login-form.scss";
+import './login-form.scss';
+import { useState } from 'react';
+import { useLogin } from './useLoginHook';
+import Spinner from '../Spinner/Spinner';
 
 export default function LoginForm() {
+  const { login, isLoading } = useLogin();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({ email: '', password: '' });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const newErrors = { email: '', password: '' };
+
+    if (!email.includes('@')) {
+      newErrors.email = 'Email musi zawierać @';
+    }
+    if (password.length < 8) {
+      newErrors.password = 'Hasło musi mieć przynajmniej 8 znaków';
+    }
+
+    if (newErrors.email || newErrors.password) {
+      setErrors(newErrors);
+      return;
+    }
+
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail('');
+          setPassword('');
+          setErrors({ email: '', password: '' });
+        },
+      }
+    );
+  };
+
+  const handleBlur = (field) => {
+    const newErrors = { ...errors, [field]: '' };
+    setErrors(newErrors);
+  };
+
   return (
     <div className="login-form">
       <h3>Zaloguj się</h3>
-      <form action="">
+      <form onSubmit={handleLogin}>
         <div className="wrapper">
-          <p>
-            <input type="email" placeholder="Email" />
-          </p>
-          <p>
-            <input type="password" placeholder="Hasło" required />
-          </p>
+          <div>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              autoComplete="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={() => handleBlur('email')}
+              disabled={isLoading}
+            />
+            {errors.email && <p className="error-text">{errors.email}</p>}
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              placeholder="Hasło"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onBlur={() => handleBlur('password')}
+              disabled={isLoading}
+            />
+            {errors.password && <p className="error-text">{errors.password}</p>}
+          </div>
         </div>
-        <p className="co-submit">
-          <input type="submit" value="Zaloguj" className="btn submit read-more" />
-        </p>
+        {isLoading ? (
+          <Spinner type="small-spinner" />
+        ) : (
+          <p className="co-submit">
+            <input
+              type="submit"
+              value="Zaloguj"
+              className="btn submit read-more"
+            />
+          </p>
+        )}
       </form>
     </div>
   );
