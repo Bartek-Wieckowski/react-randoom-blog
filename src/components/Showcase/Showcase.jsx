0b955876c7408951f7400644fa6145949a8724c2
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-16 15:23:11 +0100

    display user data in user is loggin

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -1,17 +1,36 @@
-import { Link } from "react-router-dom";
-import "./showcase.scss";
-import noSetImg from "../../assets/noSetImg.png";
+import { Link } from 'react-router-dom';
+import './showcase.scss';
+import noSetImg from '../../assets/noSetImg.png';
+import { useUser } from '../../auth/useUserHook';
 
 export default function Showcase() {
+  const { user } = useUser();
+
   return (
     <div className="profile">
-      <img src={noSetImg} alt="" />
-      <div className="name">
-          <span>Witaj</span>
-        <h3>
-          <Link to="">Username</Link>
-        </h3>
-      </div>
+      {user && user.user_metadata ? (
+        <>
+          <img src={user.user_metadata.avatar || noSetImg} alt="" />
+          <div className="name">
+            <span>Witaj</span>
+            <h3>
+              <Link to="">
+                {user.user_metadata.nickName || user.user_metadata.fullName}
+              </Link>
+            </h3>
+          </div>
+        </>
+      ) : (
+        <>
+          <img src="" alt="" className="no-visibility" />
+          <div className="no-visibility">
+            <span></span>
+            <h3>
+              <Link to=""></Link>
+            </h3>
+          </div>
+        </>
+      )}
     </div>
   );
 }
