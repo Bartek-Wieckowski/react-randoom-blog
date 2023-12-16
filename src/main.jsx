9fe1ddeb0aec03c8fef14cca740e0426fa02107d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-16 15:18:10 +0100

    add default options in query client

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,14 @@ import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
