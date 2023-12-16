dde6eef9257336654edfd8d27042034df4ef56c7
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-16 08:07:53 +0100

    seperate menu navigations

--- src/utils/navigationMenu.js
+++ src/utils/navigationMenu.js
@@ -1,55 +1,63 @@
-const menuItems = [
+export const menuItems = [
   {
-    label: "Home",
-    to: "/",
+    label: 'Home',
+    to: '/',
   },
   {
-    label: "Popularne",
-    to: "/popularne",
+    label: 'Popularne',
+    to: '/popularne',
   },
   {
-    label: "Kategorie",
+    label: 'Kategorie',
     hasChild: true,
     subItems: [
       {
-        label: "Technologia i Gadżety",
-        to: "/kategoria/technologia-i-gadzety",
-        slug: "technologia-i-gadzety",
+        label: 'Technologia i Gadżety',
+        to: '/kategoria/technologia-i-gadzety',
+        slug: 'technologia-i-gadzety',
       },
       {
-        label: "Kuchnia i Przepisy Kulinarne",
-        to: "/kategoria/kuchnia-i-przepisy-kulinarne",
-        slug: "kuchnia-i-przepisy-kulinarne",
+        label: 'Kuchnia i Przepisy Kulinarne',
+        to: '/kategoria/kuchnia-i-przepisy-kulinarne',
+        slug: 'kuchnia-i-przepisy-kulinarne',
       },
       {
-        label: "Zdrowie i Fitness",
-        to: "/kategoria/zdrowie-i-fitness",
-        slug: "zdrowie-i-fitness",
+        label: 'Zdrowie i Fitness',
+        to: '/kategoria/zdrowie-i-fitness',
+        slug: 'zdrowie-i-fitness',
       },
       {
-        label: "Moda i Styl",
-        to: "/kategoria/moda-i-styl",
-        slug: "moda-i-styl",
+        label: 'Moda i Styl',
+        to: '/kategoria/moda-i-styl',
+        slug: 'moda-i-styl',
       },
       {
-        label: "Humor i Rozrywka",
-        to: "/kategoria/humor-i-rozrywka",
-        slug: "humor-i-rozrywka",
+        label: 'Humor i Rozrywka',
+        to: '/kategoria/humor-i-rozrywka',
+        slug: 'humor-i-rozrywka',
       },
     ],
   },
   {
-    label: "Logowanie",
-    to: "/logowanie",
-  },
-  {
-    label: "Rejestracja",
-    to: "/rejestracja",
-  },
-  {
-    label: "Kontakt",
-    to: "/kontakt",
+    label: 'Kontakt',
+    to: '/kontakt',
   },
 ];
 
-export default menuItems;
+export const menuUserItems = [
+  {
+    hasUserLoggin: false,
+    label: 'Logowanie',
+    to: '/logowanie',
+  },
+  {
+    hasUserLoggin: false,
+    label: 'Rejestracja',
+    to: '/rejestracja',
+  },
+  {
+    hasUserLoggin: true,
+    label: 'Profil',
+    to: '/profil',
+  },
+];
