9c2e5306a2250746c01896cb857ddd3244161dc4
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-17 13:37:19 +0100

    use protected route component and add two route inside

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import "./styles/global.scss";
-import { Suspense, lazy, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import './styles/global.scss';
+import { Suspense, lazy, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 // pages
-const Home = lazy(() => import("./pages/Home/Home"));
-const Popular = lazy(() => import("./pages/Popular/Popular"));
-const Post = lazy(() => import("./pages/Post/Post"));
-const Category = lazy(() => import("./pages/Category/Category"));
-const Author = lazy(() => import("./pages/Author/Author"));
-const ReadTime = lazy(() => import("./pages/ReadTime/ReadTime"));
-const Login = lazy(() => import("./pages/Login/Login"));
-const Register = lazy(() => import("./pages/Register/Register"));
-const SearchFilterResults = lazy(() => import("./pages/SearchFilterResults/SearchFilterResults"));
-const PageNotFound = lazy(() => import("./pages/PageNotFound/PageNotFound"));
+const Home = lazy(() => import('./pages/Home/Home'));
+const Popular = lazy(() => import('./pages/Popular/Popular'));
+const Post = lazy(() => import('./pages/Post/Post'));
+const Category = lazy(() => import('./pages/Category/Category'));
+const Author = lazy(() => import('./pages/Author/Author'));
+const ReadTime = lazy(() => import('./pages/ReadTime/ReadTime'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Register = lazy(() => import('./pages/Register/Register'));
+const SearchFilterResults = lazy(() =>
+  import('./pages/SearchFilterResults/SearchFilterResults')
+);
+const PageNotFound = lazy(() => import('./pages/PageNotFound/PageNotFound'));
 
 // components
-const Navigation = lazy(() => import("./components/Navigation/Navigation"));
-const Footer = lazy(() => import("./components/Footer/Footer"));
-import Spinner from "./components/Spinner/Spinner";
+const Navigation = lazy(() => import('./components/Navigation/Navigation'));
+const Footer = lazy(() => import('./components/Footer/Footer'));
+import Spinner from './components/Spinner/Spinner';
 
 // contexts
-import { PostsProvider } from "./contexts/PostsContext";
-import { SearchFilterProvider } from "./contexts/SearchFilterContext";
+import { PostsProvider } from './contexts/PostsContext';
+import { SearchFilterProvider } from './contexts/SearchFilterContext';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import UserSettings from './pages/UserSettings/UserSettings';
 
 export default function App() {
   const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
 
   const isMobileDevice = () => {
-    return window.matchMedia("(max-width: 992px)").matches;
+    return window.matchMedia('(max-width: 992px)').matches;
   };
   const onOpenMobileMenu = () => {
     if (isMobileDevice()) {
@@ -36,7 +40,7 @@ export default function App() {
   };
 
   return (
-    <div id="page" className={`page ${isOpenMobileMenu ? "showmenu" : ""}`}>
+    <div id="page" className={`page ${isOpenMobileMenu ? 'showmenu' : ''}`}>
       <PostsProvider>
         <SearchFilterProvider>
           <Navigation onOpenMobileMenu={onOpenMobileMenu} />
@@ -50,7 +54,14 @@ export default function App() {
               <Route path="/czas-czytania/:value" element={<ReadTime />} />
               <Route path="/logowanie" element={<Login />} />
               <Route path="/rejestracja" element={<Register />} />
-              <Route path="/wyniki-wyszukiwania" element={<SearchFilterResults />} />
+              <Route
+                path="/wyniki-wyszukiwania"
+                element={<SearchFilterResults />}
+              />
+              <Route path="/profil/*" element={<ProtectedRoute />}>
+                <Route path="ustawienia" element={<UserSettings />} />
+                <Route path="historia" />
+              </Route>
               <Route path="*" element={<PageNotFound />} />
             </Routes>
           </Suspense>
