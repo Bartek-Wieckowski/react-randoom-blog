a859f5bd30c3041c1cc2f6c8c501e90df7b00ee5
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-12-20 21:27:13 +0100

    create function to get current post id but from supabase

--- src/services/apiComments.js
+++ src/services/apiComments.js
@@ -1,15 +1,37 @@
 import supabase, { supabaseUrl } from '../utils/supabaseConfig';
 
 export async function addCommentCurrentPostAPI(commentData) {
-  const { userName, userEmail, commentContent, postIDContentful, userID } =
-    commentData;
+  const {
+    userName,
+    userEmail,
+    commentContent,
+    postIDContentful,
+    userID,
+    postID,
+  } = commentData;
   const { data, error } = await supabase
     .from('randoomBlogComments')
-    .insert([{ userName, userEmail, commentContent, postIDContentful, userID }])
+    .insert([
+      { userName, userEmail, commentContent, postIDContentful, userID, postID },
+    ])
     .select();
 
   if (error) {
     throw new Error('Something went wrong');
   }
+  return data;
+}
+
+export async function getCurrentPostIDfromSupabase(id) {
+  const { data, error } = await supabase
+    .from('randoomBlogPosts')
+    .select('id')
+    .eq('postIDContentful', id);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Błąd podczas odczytu danych');
+  }
+
   return data;
 }
